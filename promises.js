55a1bdb3f5f1efe510b06517b14c3b9ab26c1329
grabania <grabania.work@gmail.com>
2020-05-18 23:32:07 +0100

    Add promise timeline

--- promises.js
+++ promises.js
@@ -1,1 +1,5 @@
 var sequence = get('example.json').then(doSomething).then(doSomethingElse);
+new Promise(function(resolve, reject) {
+	resolve('hi'); //works
+	resolve('bye'); //can't happen a second time
+});
