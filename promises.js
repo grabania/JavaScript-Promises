769dc986331682641859d3b6451b9ad32564ba5c
grabania <grabania.work@gmail.com>
2020-05-18 23:49:44 +0100

    Add getPresent

--- promises.js
+++ promises.js
@@ -3,3 +3,8 @@ new Promise(function(resolve, reject) {
 	resolve('hi'); //works
 	resolve('bye'); //can't happen a second time
 });
+
+const promisedPresent = getPresent();
+promisedPresent
+	.then((present) => console.log('Great present!', present))
+	.catch((error) => console.log('No present :(', error));
