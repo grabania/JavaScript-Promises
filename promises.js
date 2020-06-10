d9c6256dc34c9d04fd5279594d5181218458892e
grabania <grabania.work@gmail.com>
2020-06-10 22:44:34 +0100

    Add making appointment

--- promises.js
+++ promises.js
@@ -1,28 +1,59 @@
-var sequence = get('example.json').then(doSomething).then(doSomethingElse);
-new Promise(function(resolve, reject) {
-	resolve('hi'); //works
-	resolve('bye'); //can't happen a second time
-});
+// var sequence = get('example.json').then(doSomething).then(doSomethingElse);
+// new Promise(function(resolve, reject) {
+// 	resolve('hi'); //works
+// 	resolve('bye'); //can't happen a second time
+// });
 
-const promisedPresent = getPresent();
-promisedPresent
-	.then((present) => console.log('Great present!', present))
-	.catch((error) => console.log('No present :(', error));
+// const promisedPresent = getPresent();
+// promisedPresent
+// 	.then((present) => console.log('Great present!', present))
+// 	.catch((error) => console.log('No present :(', error));
 
-function getPresent() {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			resolve('Your present!');
-		}, 5000); // 5 seconds
-	});
-}
+// function getPresent() {
+// 	return new Promise((resolve, reject) => {
+// 		setTimeout(() => {
+// 			resolve('Your present!');
+// 		}, 5000); // 5 seconds
+// 	});
+// }
 
-getPresent().then(returnToTheShop).then(buyNewiPhone).then((iPhone) => iPhone.openTypeOfWeb());
+// getPresent().then(returnToTheShop).then(buyNewiPhone).then((iPhone) => iPhone.openTypeOfWeb());
 
-const myPromise = new Promise((resolve, reject) => {
-	setTimeout(() => resolve('Gotowe!'), 5000);
-});
+// const myPromise = new Promise((resolve, reject) => {
+// 	setTimeout(() => resolve('Gotowe!'), 5000);
+// });
 
-setTimeout(() => {
-	myPromise.then((val) => console.log(val));
-}, 6000);
+// setTimeout(() => {
+// 	myPromise.then((val) => console.log(val));
+// }, 6000);
+
+//making appointment
+
+const getUserData = () => {
+	setTimeout(() => {
+		console.log('1. get user data');
+	}, 800);
+};
+
+const validateData = () => {
+	setTimeout(() => {
+		console.log('2. validate');
+	}, 900);
+};
+
+const registerUser = () => {
+	setTimeout(() => {
+		console.log('3. register');
+	}, 400);
+};
+
+const sendEmail = () => {
+	setTimeout(() => {
+		console.log('4. send email');
+	}, 200);
+};
+
+getUserData();
+validateData();
+registerUser();
+sendEmail();
